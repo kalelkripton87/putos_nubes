4bf560605824fdf1b34446e1c8e31687eeb70af8
“kalelkripton87” <kalelkripton87@gmail.com>
2017-10-03 14:19:40 -0500

    hola putitos

---
+++ formulario.php
@@ -0,0 +1,10 @@
+ El volumen de la unidad D es Datos
+ El n£mero de serie del volumen es: C0EF-8B98
+
+ Directorio de D:\putoslocal
+
+03/10/2017  14:18    <DIR>          .
+03/10/2017  14:18    <DIR>          ..
+03/10/2017  14:18                 0 formulario.php
+               1 archivos              0 bytes
+               2 dirs  286.036.180.992 bytes libres
